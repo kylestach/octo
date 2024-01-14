b21885c878e698c821c245f45fd208ffe2180a2c
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2024-01-14 04:21:02 +0000

    Added diffusion samples in config

--- scripts/configs/octo_pretrain_config.py
+++ scripts/configs/octo_pretrain_config.py
@@ -56,6 +56,7 @@ def get_config(config_string=None):
         use_map=False,
         pred_horizon=4,
         action_dim=7,
+        n_diffusion_samples=32,
     )
 
     # We augment differently for the primary and wrist cameras
