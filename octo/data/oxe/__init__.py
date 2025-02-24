78172442f1307b3d752318e34cc02e2b0f4088ae
Kyle Stachowicz <kylestach99@gmail.com>
2025-02-23 22:37:59 -0800

    Pass arbitrary kwargs through OXE

--- octo/data/oxe/__init__.py
+++ octo/data/oxe/__init__.py
@@ -18,7 +18,7 @@ def make_oxe_dataset_kwargs(
     load_language: bool = True,
     force_recompute_dataset_statistics: bool = False,
     action_proprio_normalization_type: NormalizationType = NormalizationType.NORMAL,
-    mc_discount: float = 0.98,
+    **kwargs
 ) -> Dict[str, Any]:
     """Generates dataset kwargs for a given dataset from Open X-Embodiment. The returned kwargs can be passed
     directly into `octo.data.dataset.make_dataset_from_rlds`.
@@ -96,7 +96,7 @@ def make_oxe_dataset_kwargs(
     if force_recompute_dataset_statistics:
         dataset_kwargs["force_recompute_dataset_statistics"] = True
 
-    dataset_kwargs["mc_discount"] = mc_discount
+    dataset_kwargs.update(kwargs)
 
     return {"name": name, "data_dir": data_dir, **dataset_kwargs}
 
